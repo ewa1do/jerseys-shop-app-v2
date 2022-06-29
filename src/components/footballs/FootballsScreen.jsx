9e7213c83c3d12b3d71ad469e071cb4699e0f3bf
ewa1do <this.eduardovera@gmail.com>
2022-06-29 13:23:02 -0400

    deleting log

--- src/components/footballs/FootballsScreen.jsx
+++ src/components/footballs/FootballsScreen.jsx
@@ -2,7 +2,6 @@ import { ballsItems } from '../../data/items';
 import { ItemList } from '../items/ItemList';
 
 export const FootballsScreen = () => {
-  console.log(ballsItems);
   return (
     <div>
       <h2 className='section-title'>Footballs</h2>
