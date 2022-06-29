f73f3bfd754e61e3c32e30abbbad7cccd436d1c7
ewa1do <this.eduardovera@gmail.com>
2022-06-29 17:41:21 -0400

    including the total cart price functionality

--- src/components/cart/CartScreen.jsx
+++ src/components/cart/CartScreen.jsx
@@ -1,26 +1,35 @@
 import { useContext } from 'react';
 import { StyledItem } from '../items/styles';
 
-import { CartContext } from '../../context/cartContext';
+import { CartContext } from '../../context/cartContext.js';
 import { CartItem } from './CartItem';
 
 export const CartScreen = () => {
   const { cart } = useContext(CartContext);
+
+  const handleTotalCartPrice = () => {
+    return cart.reduce((acc, item) => acc + item.price, 0) || '';
+  };
+
+  const result = handleTotalCartPrice();
 
   return (
     <div>
       <h2 className='section-title'>Cart</h2>
 
       {cart.length > 0 ? (
-        <ul>
-          {cart.map(items => {
-            return (
-              <StyledItem key={items.id}>
-                <CartItem {...items} />
-              </StyledItem>
-            );
-          })}
-        </ul>
+        <>
+          <ul>
+            {cart.map(items => {
+              return (
+                <StyledItem key={items.id}>
+                  <CartItem {...items} />
+                </StyledItem>
+              );
+            })}
+          </ul>
+          <h3>Total: {result.toFixed(2)}</h3>
+        </>
       ) : (
         <h3>Wow such empty, add some items!</h3>
       )}
