7084e99261c64bc69cccf63e089a90c8f353cb4a
ewa1do <this.eduardovera@gmail.com>
2022-07-04 10:17:26 -0400

    creating handler buttons to navigate throught the payment screen

--- src/components/payment/PaymentScreen.jsx
+++ src/components/payment/PaymentScreen.jsx
@@ -1,15 +1,35 @@
 import { useContext } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { types } from '../../types/types';
 import { CartContext } from '../../context/cartContext';
 import { PaymentForm } from './PaymentForm';
+import { PaymentStyled } from './styles.js';
 
 export const PaymentScreen = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, dispatch } = useContext(CartContext);
   const { state } = useLocation();
   const { total = '' } = state || {};
 
+  const navigate = useNavigate();
+
+  const handleReturnToPreviousScreen = () => {
+    navigate(-1);
+  };
+
+  const handlePaymentSuccess = () => {
+    alert('Payment Successfully Submited');
+
+    dispatch({
+      type: types.clear,
+      payload: cart,
+    });
+
+    navigate('/');
+  };
+
   return (
-    <div>
+    <PaymentStyled>
       <h2>Make Your Payment</h2>
       <h4>Contact Details: </h4>
       <PaymentForm />
@@ -17,6 +37,12 @@ export const PaymentScreen = () => {
         <h5>Items Qty: {cart.length}</h5>
         <h5>Total: {total}</h5>
       </div>
-    </div>
+      <div>
+        <button onClick={handleReturnToPreviousScreen}>
+          Keep Shopping
+        </button>
+        <button onClick={handlePaymentSuccess}>Pay now!</button>
+      </div>
+    </PaymentStyled>
   );
 };
